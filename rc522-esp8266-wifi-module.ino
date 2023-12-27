289354e394b7fa826a8a522b0b90b650d7eb71aa
manuelkwizera <kwizeraemmanuel11@gmail.com>
2023-12-27 11:08:12 +0530

    creating feature to read data from the api

--- rc522-esp8266-wifi-module.ino
+++ rc522-esp8266-wifi-module.ino
@@ -1,30 +1,108 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-constexpr uint8_t RST_PIN = D3;     // Configurable, see typical pin layout above
-constexpr uint8_t SS_PIN = D4;     // Configurable, see typical pin layout above
+/* define wifi credentials */
+#define SERVER_IP "192.168.150.104"
+#ifndef STASSID
+#define STASSID "COFE_53A8"
+#define STAPSK "cofe8347"
+#endif
 
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+/* rc522 card  reader pinout */
+constexpr uint8_t RST_PIN = D3;  // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = D4;   // Configurable, see typical pin layout above
+
+MFRC522 rfid(SS_PIN, RST_PIN);  // Instance of the class
 MFRC522::MIFARE_Key key;
 
+/* this variable hold the serial number of the rfid card */
 String tag;
 
 void setup() {
-  Serial.begin(9600);
-  SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522
+  Serial.begin(115200);
+  SPI.begin();      // Init SPI bus
+  rfid.PCD_Init();  // Init MFRC522
+
+  /* connect node mcu board to wifi */
+  WiFi.begin(STASSID, STAPSK);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  if ( ! rfid.PICC_IsNewCardPresent())
-    return;
-  if (rfid.PICC_ReadCardSerial()) {
+  String tag = scanRfid();
+
+  if (!tag.isEmpty()) {
+    Serial.println("");
+    Serial.print("CARD DETECTED " + tag);
+    Serial.println("");
+    delay(1000);
+    Serial.println("PROCESSING...");
+    delay(2000);
+    postRequest(tag);
+  }
+
+  Serial.println("");
+  Serial.print("TAP THE CARD...");
+  delay(5000);
+}
+
+String scanRfid() {
+  String tag = "";
+
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
     for (byte i = 0; i < 4; i++) {
       tag += rfid.uid.uidByte[i];
     }
     Serial.println(tag);
-    tag = "";
     rfid.PICC_HaltA();
     rfid.PCD_StopCrypto1();
   }
+
+  return tag;
 }
+
+void postRequest(String data) {
+  // Ensure there is a valid WiFi connection
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("[HTTP] No WiFi connection");
+    return;
+  }
+
+  WiFiClient client;
+  HTTPClient http;
+
+  Serial.print("[HTTP] begin...\n");
+  // configure traged server and url
+  http.begin(client, "http://" + String(SERVER_IP) + "/kwanzaa-pay/public/api/post-request");
+  http.addHeader("Content-Type", "application/json");
+
+  Serial.print("[HTTP] POST...\n");
+  // start connection and send HTTP header and body
+  int httpCode = http.POST(data);
+
+  if (httpCode > 0) {
+    Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+    if (httpCode == HTTP_CODE_OK) {
+      const String& payload = http.getString();
+      Serial.println("");
+      Serial.print("SERVER RESPONSE " + payload);
+      Serial.println("");
+    } else {
+      Serial.printf("[HTTP] POST... server returned error code: %d\n", httpCode);
+    }
+  } else {
+    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+}
+
